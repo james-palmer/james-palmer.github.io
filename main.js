8178957c850d6245cade4b0f91043d43efa58f16
Glitch (meetjp) <none>
2019-09-06 02:52:32 +0000

    scroll animation and drop-downs

---
+++ main.js
@@ -0,0 +1,64 @@
+let myLabelsJCD = document.querySelectorAll('.lbl-toggle-JCD');
+
+Array.from(myLabelsJCD).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
+
+let myLabelsDL = document.querySelectorAll('.lbl-toggle-DL');
+
+Array.from(myLabelsDL).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
+
+let myLabelsSW = document.querySelectorAll('.lbl-toggle-SW');
+
+Array.from(myLabelsSW).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
+
+let myLabelsEL = document.querySelectorAll('.lbl-toggle-EL');
+
+Array.from(myLabelsEL).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
+
+let myLabelsLB = document.querySelectorAll('.lbl-toggle-LB');
+
+Array.from(myLabelsLB).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
